b9d4a0cfc46ce9c12aeaa6b748fed28b923b6574
AREKUSHISU <alexveo855@gmail.com>
2025-09-22 00:28:30 -0600

    feat: Configure local TailwindCSS v4 setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'e0e0e0': '#e0e0e0', // Custom background color
+        'bebebe': '#bebebe', // Custom dark shadow color
+      },
+      boxShadow: {
+        'neumorphic': '20px 20px 60px #bebebe, -20px -20px 60px #ffffff',
+      }
+    },
+  },
+  plugins: [],
+}
