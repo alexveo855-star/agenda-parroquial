40196ea1c81bf52ccb7be7fbff8d453366de68b7
AREKUSHISU <alexveo855@gmail.com>
2025-09-25 02:31:15 -0600

    refactor: Remove temporary debug log for Firebase config

--- scripts/create-config.js
+++ scripts/create-config.js
@@ -23,7 +23,6 @@ let fileContent;
 
 if (firebaseConfig.apiKey && firebaseConfig.projectId) {
   const configJsonString = JSON.stringify(firebaseConfig);
-  console.log('DEBUG: configJsonString generado:', configJsonString);
   fileContent = `const __firebase_config = '${configJsonString}';`;
   console.log('El archivo public/firebase-config.js fue creado exitosamente desde las variables de entorno.');
 } else {
